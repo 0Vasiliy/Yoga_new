b98ae93b1e61b1dafba7fda34f0471156da1998f
Козин-Поздняков Василий <vasiliy_pit@mail.ru>
2022-09-19 23:16:46 +0300

    Модальное окно закончено

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
-window.addEventListener('DOMContentLoaded', function() {
+window.addEventListener('DOMContentLoaded', function() { // Это событие срабатывает,когда полностью загрузилась DOM структура документа
 
     'use strict';
-    let tab = document.querySelectorAll('.info-header-tab'),
+    let tab = document.querySelectorAll('.info-header-tab'), // Объявление переменных
         info = document.querySelector('.info-header'),
         tabContent = document.querySelectorAll('.info-tabcontent');
 
-    function hideTabContent(a) {
+    function hideTabContent(a) {                             // Функция которая скрывает табы
         for (let i = a; i < tabContent.length; i++) {
             tabContent[i].classList.remove('show');
             tabContent[i].classList.add('hide');
@@ -14,14 +14,14 @@ window.addEventListener('DOMContentLoaded', function() {
 
     hideTabContent(1);
 
-    function showTabContent(b) {
+    function showTabContent(b) {                              // Функция показывает определённый таб контент      
         if (tabContent[b].classList.contains('hide')) {
             tabContent[b].classList.remove('hide');
             tabContent[b].classList.add('show');
         }
     }
 
-    info.addEventListener('click', function(event) {
+    info.addEventListener('click', function(event) {           //Назначение обработчика событий при клике на каждый из табов
         let target = event.target;
         if (target && target.classList.contains('info-header-tab')) {
             for(let i = 0; i < tab.length; i++) {
@@ -34,4 +34,73 @@ window.addEventListener('DOMContentLoaded', function() {
         }
 
     });
-});
+
+    // Timer 
+
+    let deadline = '2022-09-19';
+
+    function getTimeRemaining(endtime) {                     // Функция определяет остаток времени
+        let t = Date.parse(endtime) - Date.parse(new Date()),
+        seconds = Math.floor((t/1000) % 60),
+        minutes = Math.floor((t/1000/60) % 60),
+        hours = Math.floor((t/(1000*60*60)));
+
+        return {                                           // Возвращает объекты
+            'total' : t,
+            'hours' : hours,
+            'minutes' : minutes,
+            'seconds' : seconds
+        };
+    }
+
+    function setClock(id, endtime) {
+        let timer = document.getElementById(id),            // Функция выставляет и запускает часы
+            hours = timer.querySelector('.hours'),
+            minutes = timer.querySelector('.minutes'),
+            seconds = timer.querySelector('.seconds'),
+            timeInterval = setInterval(updateClock, 1000);
+            
+        function updateClock() {                             // Функция обновляет часы каждую секунду
+            let t = getTimeRemaining(endtime);
+
+            function addZero(num){
+                        if(num <= 9) {
+                            return '0' + num;
+                        } else return num;
+                    };
+
+            hours.textContent = addZero(t.hours);
+            minutes.textContent = addZero(t.minutes);
+            seconds.textContent = addZero(t.seconds);
+
+            if (t.total <= 0) {
+                clearInterval(timeInterval);
+                hours.textContent = '00';
+                minutes.textContent = '00';
+                seconds.textContent = '00';
+            }
+        }
+
+    }
+
+    setClock('timer', deadline);
+ // Modal
+
+    let more = document.querySelector('.more'),
+        overlay = document.querySelector('.overlay'),
+        close = document.querySelector('.popup-close');
+
+        more.addEventListener('click', function() {
+        overlay.style.display = 'block';
+        this.classList.add('more-splash');
+        document.body.style.overflow = 'hidden';
+    });
+
+        close.addEventListener('click', function() {
+        overlay.style.display = 'none';
+        more.classList.remove('more-splash');
+        document.body.style.overflow = '';
+    });
+        });
+    
+
